c73ba2bec51461e61712d16658d6313e3c5502ac
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-07-08 20:11:52 +0300

    Summary Component is done

--- weather-forecast/src/App.vue
+++ weather-forecast/src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { API_KEY, BASE_URL } from './constants';
 import Highlights from './components/Highlights.vue';
-import WeatherSummary from './components/WeatherSummary.vue'
+import WeatherSummary from './components/WeatherSummary.vue';
+
+const city = ref('Paris');
+const weatherInfo = ref(null);
+
+const getWeather = () => {
+  fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => (weatherInfo.value = data));
+};
+
+onMounted(getWeather);
 </script>
 
 <template>
@@ -12,20 +25,23 @@ import WeatherSummary from './components/WeatherSummary.vue'
             <section class="section section-left">
               <div class="info">
                 <div class="city-inner">
-                  <input type="text" class="search">
+                  <input
+                    v-model="city"
+                    type="text"
+                    class="search"
+                    @keyup="getWeather"
+                  />
                 </div>
-                <WeatherSummary />
+                <WeatherSummary :weatherInfo="weatherInfo" />
               </div>
             </section>
             <section class="section section-right">
               <Highlights />
-            </section> 
+            </section>
           </div>
           <div class="sections">
             <section class="section-bottom">
-              <div
-                class="block-bottom"
-              >
+              <div class="block-bottom">
                 <div class="block-bottom-inner">
                   <div class="block-bottom-pic pic-coords"></div>
                   <div class="block-bottom-texts">
@@ -34,7 +50,8 @@ import WeatherSummary from './components/WeatherSummary.vue'
                         Longitude: 2.3488
                       </div>
                       <div class="block-bottom-text-block-desc">
-                        Longitude measures distance east or west of the prime meridian.
+                        Longitude measures distance east or west of the prime
+                        meridian.
                       </div>
                     </div>
                     <div class="block-bottom-text-block">
@@ -42,7 +59,8 @@ import WeatherSummary from './components/WeatherSummary.vue'
                         Latitude: 48.8534
                       </div>
                       <div class="block-bottom-text-block-desc">
-                        Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator. 
+                        Latitude lines start at the equator (0 degrees latitude)
+                        and run east and west, parallel to the equator.
                       </div>
                     </div>
                   </div>
@@ -50,9 +68,7 @@ import WeatherSummary from './components/WeatherSummary.vue'
               </div>
             </section>
             <section class="section-bottom">
-              <div
-                class="block-bottom"
-              >
+              <div class="block-bottom">
                 <div class="block-bottom-inner">
                   <div class="block-bottom-pic pic-humidity"></div>
                   <div class="block-bottom-texts">
@@ -61,9 +77,12 @@ import WeatherSummary from './components/WeatherSummary.vue'
                         Humidity: 60 %
                       </div>
                       <div class="block-bottom-text-block-desc">
-                        Humidity is the concentration of water vapor present in the air. Water vapor, the gaseous state of water, is generally invisible to the human eye.
+                        Humidity is the concentration of water vapor present in
+                        the air. Water vapor, the gaseous state of water, is
+                        generally invisible to the human eye.
                         <br /><br />
-                        The same amount of water vapor results in higher relative humidity in cool air than warm air.
+                        The same amount of water vapor results in higher
+                        relative humidity in cool air than warm air.
                       </div>
                     </div>
                   </div>
